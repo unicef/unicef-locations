76d10a6bb2eee313fd0eef9381de951537bb1737
sax <s.apostolico@gmail.com>
2018-07-11 16:46:48 +0200

    updates - UNSTABLE

---
+++ src/unicef_locations/migrations/0002_fix_null_values.py
@@ -0,0 +1,30 @@
+from django.db import migrations
+from unicef_locations.libs import fix_null_values
+
+
+def fix_nulls(apps, schema):
+    # Change null values in these fields to empty strings
+    fix_null_values(
+        apps.get_model('locations.cartodbtable'),
+        [
+            'color',
+            'display_name',
+            'parent_code_col',
+        ]
+    )
+    fix_null_values(
+        apps.get_model('locations.location'),
+        [
+            'p_code',
+        ]
+    )
+
+
+class Migration(migrations.Migration):
+    dependencies = [
+        (u'locations', u'0001_initial'),
+    ]
+
+    operations = [
+        migrations.RunPython(fix_nulls, migrations.RunPython.noop)
+    ]
