b3faee40b0673f3e2e48538045876e2184cbf885
Domenico DiNicola <dom.dinicola@gmail.com>
2018-07-25 12:41:59 -0400

    tweak location viewset

--- src/unicef_locations/views.py
+++ src/unicef_locations/views.py
@@ -68,7 +68,7 @@ class LocationsLightViewSet(mixins.ListModelMixin, viewsets.GenericViewSet):
     """
     Returns a list of all Locations with restricted field set.
     """
-    queryset = Location.objects.all()
+    queryset = Location.objects.defer('geom', )
     serializer_class = LocationLightSerializer
 
     @etag_cached('locations')
@@ -82,7 +82,7 @@ class LocationQuerySetView(ListAPIView):
 
     def get_queryset(self):
         q = self.request.query_params.get('q')
-        qs = self.model.objects
+        qs = self.model.objects.defer('geom', )
 
         if q:
             qs = qs.filter(name__icontains=q)
