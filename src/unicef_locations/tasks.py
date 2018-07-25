2514ac07c2c34b560664d188fc35bf9a56a0c2c9
denes csaba <csaba.denes@nordlogic.com>
2018-07-25 20:51:03 +0300

    This change will hopefully solve deadlock issues during location import on multi-container environments

--- src/unicef_locations/tasks.py
+++ src/unicef_locations/tasks.py
@@ -175,6 +175,10 @@ def update_sites_from_cartodb(carto_table_pk):
     else:
         # wrap Location tree updates in a transaction, to prevent an invalid tree state due to errors
         with transaction.atomic():
+            # should write lock the locations table until the tree is rebuilt
+            # should prevent issues like https://sentry.io/unicef-jk/etools-staging/issues/618355875/
+            Location.objects.select_for_update().only('id')
+
             # disable tree 'generation' during single row updates, rebuild the tree after.
             # this should prevent errors happening (probably)due to invalid intermediary tree state
             with Location.objects.disable_mptt_updates():
