6270334ca8a6c12904b329e2c766d0b1dd6df1ae
denes csaba <csaba.denes@nordlogic.com>
2018-08-03 19:31:21 +0300

    port over some of the changes from locations remap

--- src/unicef_locations/models.py
+++ src/unicef_locations/models.py
@@ -5,6 +5,8 @@ from django.contrib.gis.db import models
 from django.db.models.signals import post_delete, post_save
 from django.dispatch.dispatcher import receiver
 from django.utils.translation import ugettext as _
+from django.contrib.contenttypes.fields import GenericForeignKey
+from django.contrib.contenttypes.models import ContentType
 from model_utils.fields import AutoCreatedField, AutoLastModifiedField
 from mptt.managers import TreeManager
 from mptt.models import MPTTModel, TreeForeignKey
@@ -31,10 +33,16 @@ class GatewayType(models.Model):
         return self.name
 
 
-class LocationManager(TreeManager):
-
+class ActiveLocationsManager(TreeManager):
     def get_queryset(self):
-        return super(LocationManager, self).get_queryset().order_by('name').select_related('gateway')
+        return super(ActiveLocationsManager, self).get_queryset().filter(is_active=True)\
+            .order_by('name').select_related('gateway')
+
+
+class AllLocationsManager(TreeManager):
+    def get_queryset(self):
+        return super(AllLocationsManager, self).get_queryset()\
+            .order_by('name').select_related('gateway')
 
 
 class Location(MPTTModel):
@@ -82,10 +90,12 @@ class Location(MPTTModel):
         blank=True,
     )
     point = models.PointField(verbose_name=_("Point"), null=True, blank=True)
+    is_active = models.BooleanField(verbose_name=_("Active"), default=True, blank=True)
     created = AutoCreatedField(_('created'))
     modified = AutoLastModifiedField(_('modified'))
 
-    objects = LocationManager()
+    objects = ActiveLocationsManager()
+    all_locations = AllLocationsManager()
 
     def __str__(self):
         # TODO: Make generic
@@ -111,6 +121,35 @@ class Location(MPTTModel):
         unique_together = ('name', 'gateway', 'p_code')
         ordering = ['name']
         app_label = 'locations'
+
+
+class LocationRemapHistory(models.Model):
+    '''
+    Location Remap History records for the related objects(interventions, travels, activities, actions)
+    '''
+    old_location = models.ForeignKey(
+        Location,
+        verbose_name=_("Old Location"),
+        on_delete=models.CASCADE,
+        related_name="+"
+    )
+    new_location = models.ForeignKey(
+        Location,
+        verbose_name=_("New Location"),
+        on_delete=models.CASCADE,
+        related_name="+"
+    )
+
+    content_type = models.ForeignKey(
+        ContentType,
+        verbose_name=_('Content Type'),
+        on_delete=models.CASCADE
+    )
+    object_id = models.IntegerField(verbose_name=_('Object ID'))
+    content_object = GenericForeignKey('content_type', 'object_id')
+
+    comment = models.TextField(verbose_name=_('Comments'), blank=True)
+    created = AutoCreatedField(_('created'))
 
 
 @receiver(post_delete, sender=Location)
@@ -139,6 +178,8 @@ class CartoDBTable(MPTTModel):
     )
     name_col = models.CharField(max_length=254, default='name', verbose_name=_('Name Column'))
     pcode_col = models.CharField(max_length=254, default='pcode', verbose_name=_('Pcode Column'))
+    # Cartodb table name used to remap old pcodes to new pcodes
+    remap_table_name = models.CharField(max_length=254, verbose_name=_('Remap Table Name'), blank=True, null=True)
     parent_code_col = models.CharField(max_length=254, default='', blank=True, verbose_name=_('Parent Code Column'))
     parent = TreeForeignKey(
         'self', null=True, blank=True, related_name='children', db_index=True,
